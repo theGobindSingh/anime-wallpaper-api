da92cb2df18307c9799545fde80c8ec29f2d1340
Sai Rohit <sairohit11.s@gmail.com>
2024-11-01 23:57:39 +0530

    config changes

--- next.config.js
+++ next.config.js
@@ -8,16 +8,18 @@ const nextConfig = {
     return [
       {
         // matching all API routes
-        source: '/api/:path*',
+        source: '/api/:path',
         headers: [
-          { key: 'Access-Control-Allow-Origin', value: '*' }, // Allow all origins
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '' }, // replace this your actual origin
           {
             key: 'Access-Control-Allow-Methods',
-            value: 'GET, POST, PUT, DELETE, OPTIONS',
+            value: 'GET,DELETE,PATCH,POST,PUT',
           },
           {
             key: 'Access-Control-Allow-Headers',
-            value: 'Content-Type, Authorization',
+            value:
+              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
           },
         ],
       },
