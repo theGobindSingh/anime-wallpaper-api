7ecb6c024c026f3638d0c2b4a4f5e155abe1b52d
Sai Rohit <sairohit11.s@gmail.com>
2024-11-01 23:49:09 +0530

    change config

--- next.config.js
+++ next.config.js
@@ -4,6 +4,25 @@ const nextConfig = {
   images: {
     domains: [],
   },
+  headers: async () => {
+    return [
+      {
+        // matching all API routes
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Origin', value: '*' }, // Allow all origins
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET, POST, PUT, DELETE, OPTIONS',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'Content-Type, Authorization',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
