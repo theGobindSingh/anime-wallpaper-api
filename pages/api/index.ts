08f6c66af3e7b70defbb8f2c1baf929de906f63c
theGobindSingh <thesinghgobind@gmail.com>
2024-11-02 05:52:00 +0530

    changes

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -4,6 +4,14 @@ import { NextApiHandler } from 'next';
 
 const handler: NextApiHandler<ApiResponse> = async (req, res) => {
   if (req.method === 'GET') {
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,DELETE,PATCH,POST,PUT');
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, \
+Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+    );
     try {
       const katputliRes = await katputli(req.query as Query);
       return res.status(200).json({ isSuccessful: true, data: katputliRes });
